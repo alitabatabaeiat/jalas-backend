50441ee2c1349c000c609679399eaeb2d57c741d
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-11-27 17:30:18 +0330

    env validation on server start

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import {createConnection} from "typeorm";
+import * as Joi from '@hapi/joi';
 
 import App from "./app";
 import config from "./ormconfig";
+import envSchema from "./validations/env";
 
 (async () => {
     try {
+        const {error} = Joi.validate(process.env, envSchema, {allowUnknown: true});
+        if (error) {
+            console.error(error.details);
+            process.exit(1);
+        }
         await createConnection(config);
         const controllers = [];
         const app = new App(controllers);
