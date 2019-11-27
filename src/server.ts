bcdf735592002d662100285103b5562f90157081
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-11-27 17:07:47 +0330

    orm config added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import 'dotenv/config';
 import 'reflect-metadata';
+import {createConnection} from "typeorm";
 
 import App from "./app";
+import config from "./ormconfig";
 
 (async () => {
-    const controllers = [];
-    const app = new App(controllers);
-    app.listen();
+    try {
+        await createConnection(config);
+        const controllers = [];
+        const app = new App(controllers);
+        app.listen();
+    } catch (error) {
+        console.log('Error while connecting to the database', error);
+        return error;
+    }
 })();
 
