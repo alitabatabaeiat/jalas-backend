05de3b284b6ee5a8c01ad88c58e34a7184d89678
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-12-01 14:30:30 +0330

    some improvements and relationships added

--- src/entities/meetingTime.ts
+++ src/entities/meetingTime.ts
@@ -1,13 +1,18 @@
-import {Column, Entity, ManyToOne} from "typeorm";
-import BaseEntity from "./base";
+import {Column, CreateDateColumn, Entity, ManyToOne, UpdateDateColumn} from "typeorm";
 import Poll from "./poll";
 
 @Entity('meeting_times')
-export default class MeetingTime extends BaseEntity {
+export default class MeetingTime {
+    @CreateDateColumn({name: 'created_at'})
+    public createdAt: Date;
+
+    @UpdateDateColumn({name: 'updated_at'})
+    public updatedAt: Date;
+
     @Column('timestamp with time zone', {name: 'starts_at'})
     public startsAt: string;
 
-    @Column('timestamp with time zone', {name: 'starts_at'})
+    @Column('timestamp with time zone', {name: 'ends_at'})
     public endsAt: number;
 
     @Column('smallint', {name: 'vote_for', default: 0})
@@ -16,6 +21,6 @@ export default class MeetingTime extends BaseEntity {
     @Column('smallint', {name: 'vote_against', default: 0})
     public voteAgainst: number;
 
-    @ManyToOne(type => Poll, poll => poll.possibleMeetingTimes, {onDelete: 'CASCADE'})
+    @ManyToOne(type => Poll, poll => poll.possibleMeetingTimes, {primary: true, onDelete: 'CASCADE'})
     public poll: Poll;
 }
--- src/entities/poll.ts
+++ src/entities/poll.ts
@@ -1,16 +1,17 @@
-import {Column, Entity, OneToMany, OneToOne} from "typeorm";
+import {Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne} from "typeorm";
 import BaseEntity from "./base";
 import MeetingTime from "./meetingTime";
+import User from "./user";
 
 @Entity('polls')
 export default class Poll extends BaseEntity {
     @Column({length: 30})
     public title: string;
 
-    @Column('smallint')
+    @Column('smallint', {nullable: true})
     public room: number;
 
-    @Column('smallint')
+    @Column('smallint', {default: 0})
     public state: number;
 
     @Column('timestamp with time zone', {name: 'room_requested_at'})
@@ -19,6 +20,14 @@ export default class Poll extends BaseEntity {
     @OneToMany(type => MeetingTime, meetingTime => meetingTime.poll)
     public possibleMeetingTimes: MeetingTime[];
 
-    @OneToOne(type => MeetingTime)
+    @OneToOne(type => MeetingTime, {nullable: true})
+    @JoinColumn()
     public meetingTime: MeetingTime;
+
+    @ManyToOne(type => User)
+    public owner: User;
+
+    @ManyToMany(type => User)
+    @JoinTable()
+    public participants: User[];
 }
