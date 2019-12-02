6bc1c34938e265730f2a5b52c87c4817ea0850dd
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-12-02 11:54:15 +0330

    possibleMeetingTimes rule changed

--- src/validations/poll.ts
+++ src/validations/poll.ts
@@ -5,19 +5,21 @@ const rules = {
     room: Joi.number().positive(),
     state: Joi.number().min(0).max(3),
     roomRequestedAt: Joi.date().iso(),
-    possibleMeetingTimes: Joi.array().items(
-        Joi.object({
-            startsAt: Joi.date().iso(),
-            endsAt: Joi.date().iso()
-        })
-    ).min(1),
+    possibleMeetingTimes: {
+        startsAt: Joi.date().iso(),
+        endsAt: Joi.date().iso(),
+        voteFor: Joi.number().min(0),
+        voteAgainst: Joi.number().min(0)
+    },
     meetingTimes: Joi.string().guid(),
     participants: Joi.array().items(Joi.string().email()).min(1)
 };
 
 const createPollSchema = Joi.object({
     title: rules.title.required(),
-    possibleMeetingTimes: rules.possibleMeetingTimes.required(),
+    possibleMeetingTimes: Joi.array().items(
+        Joi.object(rules.possibleMeetingTimes)
+    ).min(1).required(),
     participants: rules.participants.required()
 });
 
