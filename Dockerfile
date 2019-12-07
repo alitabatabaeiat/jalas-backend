406961b09c84f78309417ad31e7b74cf00513d9e
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-12-07 18:25:56 +0330

    docker for running server and db with just one command

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM postgres:11-alpine
-
-RUN localedef -i fa_IR -c -f UTF-8 -A /usr/share/locale/locale.alias fa_IR.UTF-8
---
+++ docker/db/001_data.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE jalas
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'fa_IR.utf8'
+    LC_CTYPE = 'fa_IR.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
