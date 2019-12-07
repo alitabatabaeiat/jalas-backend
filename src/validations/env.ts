c69fd623aa705cce599235ff531693b44f373b29
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-12-07 12:52:14 +0330

    add @types to development dependencies + new fields to env validation schema

--- src/validations/env.ts
+++ src/validations/env.ts
@@ -6,7 +6,13 @@ const envSchema = Joi.object({
     POSTGRES_PORT: Joi.number().port().required(),
     POSTGRES_USER: Joi.string().required(),
     POSTGRES_PASSWORD: Joi.string().required(),
-    POSTGRES_DB: Joi.string().required()
+    POSTGRES_DB: Joi.string().required(),
+    EMAIL_SERVICE: Joi.string().required(),
+    EMAIL_ADDRESS: Joi.string().email().required(),
+    EMAIL_PASSWORD: Joi.string().required(),
+    GMAIL_CLIENT_ID: Joi.string().required(),
+    GMAIL_CLIENT_SECRET: Joi.string().required(),
+    GMAIL_REFRESH_TOKEN: Joi.string().required()
 });
 
 export default envSchema;
