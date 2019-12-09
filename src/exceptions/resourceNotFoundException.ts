9c5e1e32971016a5489e5ab6ab7a8477be8e5bec
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-12-09 15:36:17 +0330

    small change to exception message

--- src/exceptions/resourceNotFoundException.ts
+++ src/exceptions/resourceNotFoundException.ts
@@ -3,7 +3,7 @@ import HttpException from "./httpException";
 class ResourceNotFoundException extends HttpException {
     constructor(resourceName: string, fieldName?: string, fieldValue?: string) {
         if (fieldName && fieldValue)
-            super(404, `${resourceName} with ${fieldName}: ${fieldValue} not found`);
+            super(404, `${resourceName} with ${fieldName}: '${fieldValue}' not found`);
         else
             super(404, `${resourceName} not found`);
         this.name = 'ResourceNotFoundException'
