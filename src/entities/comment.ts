91dcb4e13538cfd98f43a106f453e5a9881aa9d4
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-12-22 13:06:58 +0330

    add comment entity

---
+++ src/entities/comment.ts
@@ -0,0 +1,16 @@
+import Base from "./base";
+import { Entity, Column, OneToOne } from "typeorm";
+import Poll from "./poll";
+import User from "./user";
+
+@Entity('comments')
+export default class Comment extends Base {
+    @Column( 'text')
+    public text: Date;
+
+    @OneToOne(type => User)
+    public writer: User;
+
+    @OneToOne(type => Poll, poll => poll.comments, {onDelete: 'CASCADE'})
+    public poll: Poll;
+}
